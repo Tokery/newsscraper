3a7704489627dc0d4fdd8e8464b2f4938eaf1b39
Tokery <kevincott97@gmail.com>
2018-03-15 13:47:17 -0400

    Ultra basic API

--- scraper/app/controllers/articles_controller.rb
+++ scraper/app/controllers/articles_controller.rb
@@ -1,5 +1,16 @@
 class ArticlesController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def create
-        render plain: params[:headlines].inspect # hash with key plain
+        #render plain: params[:headlines].inspect # hash with key plain
+        puts params[:article].inspect
+        @article = Article.new(article_params)
+
+        @article.save
+        render plain: "OK"
     end
+
+    private 
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
 end
