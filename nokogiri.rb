17daf59abe1e55b354d77029a6e253f6de7abd9a
Tokery <kevincott97@gmail.com>
2018-03-20 13:26:47 -0400

    Scraper pushes actual headlines

--- nokogiri.rb
+++ nokogiri.rb
@@ -12,10 +12,14 @@ def bloomberg_trending ()
     #puts page.css('ul.top-news-v3__stories li')[0].css('article h1').text
     #puts page.xpath("//ul[@class='top-news-v3__stories']")
     stories = page.css('ul.top-news-v3__stories li')
+
+    headlines = []
     stories.each { |story| 
-        puts story.css('article h1').text
+        headline = story.css('article h1').text
+        headlines.push(headline)
     }
-    sendDataToApi(stories[0])
+    puts "Done bloomberg collection"
+    sendDataToApi(headlines)
 end
 
 def sendDataToApi (stories)
@@ -23,7 +27,7 @@ def sendDataToApi (stories)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new("/articles")
     request.add_field('Content-Type', 'application/json')
-    request.body = {'article' => {'title' => 'From scraper', 'text' => stories}}.to_json
+    request.body = {'article' => {'title' => 'Bloomberg', 'text' => stories}}.to_json
     response = http.request(request)
 end
 
