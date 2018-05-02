69c7278f627146ea975bc6acb17c0f3587158775
kevincott97@gmail.com <kevincott97@gmail.com>
2018-05-01 21:40:10 -0400

    Status messages are now sent to Telegram!

--- nokogiri.rb
+++ nokogiri.rb
@@ -6,9 +6,19 @@ require 'json'
 
 cmd_args = ARGV
 $post_to_prod = false;
-if (cmd_args.length > 0 && cmd_args[0] == "true" )
-    $post_to_prod = true
+$telegram_token = nil;
+$chat_id = nil;
+if (cmd_args.length > 0)
+    if (cmd_args[0] == "true")
+        $post_to_prod = true
+    end
+    if (cmd_args[1])
+        #Should probably be environment variables
+        $telegram_token = cmd_args[1]
+        $chat_id = cmd_args[2]
+    end
 end
+
 $post_url = $post_to_prod ? "https://obscure-inlet-97748.herokuapp.com/" : "http://localhost:3000/"
 
 def bloomberg_trending ()
@@ -48,13 +58,26 @@ def sendDataToApi (stories, urls)
     request = Net::HTTP::Post.new("/articles")
     request.add_field('Content-Type', 'application/json')
     request.body = {'article' => {'title' => 'Bloomberg', 'text' => stories.to_json, 'url' => urls.to_json}}.to_json
+    time = Time.new
     begin
         response = http.request(request)
     rescue Exception => e
-        puts "Did not POST due to exception"
-        puts e.message
+        sendLogToTelegram("#{time}: Did not POST due to exception #{e.message}")
+    else
+        sendLogToTelegram("#{time}: Response #{response.code} #{response.message}")
     end
-    puts "Response #{response.code} #{response.message}"
+end
+
+def sendLogToTelegram(message) 
+    base_url = "https://api.telegram.org/bot"
+    url = "#{base_url}#{$telegram_token}/sendMessage?chat_id=#{$chat_id}&text=#{message}"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    response = Net::HTTP.get(uri)
+    puts "Response #{response}"
+
 end
 
 bloomberg_trending()
